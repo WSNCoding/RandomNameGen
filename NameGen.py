43ec9e6322f501be9c1f572c3599ea62da6cfe87
Barry <formaggio@hotmail.co.uk>
2016-11-09 20:27:34 +0000

    M8 no scope
    Added more letters to database thing.... Please check them

--- NameGen.py
+++ NameGen.py
@@ -1,11 +1,11 @@
 letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','q','r','s','t','u','v','w','x','y','z',"'",]
-NextA = ['b','c','d','e','f','h','g','i','k','l','m','n','o','p','r','s','t']
+NextA = ['b','c','d','e','f','h','g','i','k','l','m','n','o','p','r','s','t','u']
 NextB = ['a','b','e','i','o','u','y']
 NextC = ['a','e','h','i','o','u']
 NextD = ['a','d','e','i','o','u','y']
 NextE = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
-NextF
-NextG
+NextF = ['a','e','i','l','o','r']#not finished
+NextG = ['a','e','r']#nit finished
 NextH
 NextI
 NextJ
@@ -21,7 +21,7 @@ NextS
 NextT
 NextU
 NextV
-NextW
+NextW = ['a','e','i','y']#please check ones from here down if possible sincerely Jefff
 NextX = ['e']
-NextY
-NextZ
+NextY = ['l','a','a','v']
+NextZ = ['a','i','o','y']
