9488b74d684e66a9a4a7d6b4cdc446df2cbad989
Oscar de Vince <odevince@yahoo.com>
2016-11-09 18:34:40 +0000

    Add files via upload

---
+++ NameGen.py
@@ -0,0 +1,27 @@
+letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','q','r','s','t','u','v','w','x','y','z',"'",]
+NextA = ['b','c','d','e','f','h','g','i','k','l','m','n','o','p','r','s','t']
+NextB = ['a','b','e','i','o','u','y']
+NextC = ['a','e','h','i','o','u']
+NextD = ['a','d','e','i','o','u','y']
+NextE = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','p'
+NextF
+NextG
+NextH
+NextI
+NextJ
+NextK
+NextL
+NextM
+NextN
+NextO
+NextP
+NextQ
+NextR
+NextS
+NextT
+NextU
+NextV
+NextW
+NextX = ['e']
+NextY
+NextZ
