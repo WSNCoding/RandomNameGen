a6ea3aa54046777bcc482ecd2203e2db442518ff
Oscar de Vince <odevince@yahoo.com>
2017-02-07 09:23:08 +0000

    complete following letters
    all incomplete entries were amended and the arrays are now complete

--- NameGen.py
+++ NameGen.py
@@ -1,26 +1,30 @@
+import random
+
+#uraqt
+
 letters = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','q','r','s','t','u','v','w','x','y','z',"'",]
 NextA = ['b','c','d','e','f','h','g','i','k','l','m','n','o','p','r','s','t','u']
 NextB = ['a','b','e','i','o','u','y']
 NextC = ['a','e','h','i','o','u']
 NextD = ['a','d','e','i','o','u','y']
 NextE = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
-NextF = ['a','e','i','l','o','r']#not finished
-NextG = ['a','e','r']#nit finished
-NextH
-NextI
-NextJ
-NextK
-NextL
-NextM
-NextN
-NextO
-NextP
-NextQ
-NextR
-NextS
-NextT
-NextU
-NextV
+NextF = ['a','e','i','l','o','r']
+NextG = ['a','e','r','l','o','u']
+NextH = ['a','i','o']
+NextI = ['a','i','o']
+NextJ = ['a','n']
+NextK = []
+NextL = ['a','e','i','o','u','y']
+NextM = ['a','e','i','o','u','y']
+NextN = []
+NextO = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
+NextP = ['a','e','i','o','u','y',
+NextQ = ['u']
+NextR = ['a','e','i','o','u','y']
+NextS = ['a','e','i','o','u','t','v','h']
+NextT = ['a','e','i','o','u','y']
+NextU = ['r','g']
+NextV = ['a','i']
 NextW = ['a','e','i','y']#please check ones from here down if possible sincerely Jefff
 NextX = ['e']
 NextY = ['a','i','l','v']
